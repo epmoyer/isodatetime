3fabf6324491873f18c8c8016b31825e0c29bff5
Eric Moyer <eric@lemoncrab.com>
2023-01-20 10:24:22 -0800

    wip

--- demo.py
+++ demo.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
 # Standard Library
-from isodatetime import *
+import datetime
 from dateutil import tz
 
 # Library
 from rich.console import Console
 from rich.theme import Theme
 
+# Local
+from isodatetime import (
+    datetime_to_iso_datetime,
+    datetime_to_iso_date,
+    iso_date_to_datetime,
+    iso_date_to_date,
+    iso_now,
+)
 # --------------------
 # Rich output console
 # --------------------
@@ -25,18 +33,45 @@ print = CONSOLE.print
 pprint = CONSOLE_PPRINT.print
 
 
-#fmt:off
+# fmt:off
 TEST_CASES = [
-    # {
-    #     'heading': "datetime_to_iso_datetime()",
-    #     'expression': "isodatetime.datetime_to_iso_datetime.",
-    # },
-    # {
-    #     'expression': "isodatetime.iso_now(tz=tz.gettz('UTC'))",
-    # },
-    # {
-    #     'expression': "isodatetime.iso_now(microseconds=True)",
-    # },
+    # -------------------------------
+    # datetime_to_iso_datetime()
+    # -------------------------------
+    {
+        'heading': "datetime_to_iso_datetime()",
+        'expression': "datetime_to_iso_datetime(datetime.datetime(2022, 5, 6, 12, 34, 56))",
+    },
+    {
+        'expression': "datetime_to_iso_datetime(datetime.datetime(2022, 5, 6, 12, 34, 56), tz=tz.gettz('UTC'))",
+    },
+    {
+        'expression': "datetime_to_iso_datetime(datetime.datetime(2022, 5, 6, 12, 34, 56, 9876), microseconds=True)",
+    },
+    # -------------------------------
+    # datetime_to_iso_date()
+    # -------------------------------
+    {
+        'heading': "datetime_to_iso_date()",
+        'expression': "datetime_to_iso_date(datetime.datetime(2022, 5, 6, 12, 34, 56))",
+    },
+    # -------------------------------
+    # iso_date_to_datetime()
+    # -------------------------------
+    {
+        'heading': "iso_date_to_datetime()",
+        'expression': "iso_date_to_datetime('2022-05-06')",
+    },
+    # -------------------------------
+    # iso_date_to_date()
+    # -------------------------------
+    {
+        'heading': "iso_date_to_date()",
+        'expression': "iso_date_to_date('2022-05-06')",
+    },
+    # -------------------------------
+    # iso_now()
+    # -------------------------------
     {
         'heading': "iso_now()",
         'expression': "iso_now()",
@@ -47,12 +82,16 @@ TEST_CASES = [
     {
         'expression': "iso_now(microseconds=True)",
     },
+    # -------------------------------
+    # iso_date_to_datetime()
+    # -------------------------------
     {
         'heading': "iso_date_to_datetime()",
         'expression': "iso_date_to_datetime('2022-03-04')"
     },
 ]
-#fmt:on
+# fmt:on
+
 
 def main():
     for i, case in enumerate(TEST_CASES):
