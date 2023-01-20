a68bba6a3b94f4aebf26f839e4e25d1209ae953c
Eric Moyer <eric@lemoncrab.com>
2023-01-20 07:50:17 -0800

    wip

--- demo.py
+++ demo.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python
-import isodatetime 
+import isodatetime
+from dateutil import tz
 
 #fmt:off
 TEST_CASES = [
     {
         'expression': "isodatetime.iso_now()"
-    }
+    },
+    {
+        'expression': "isodatetime.iso_now(tz=tz.gettz('UTC'))"
+    },
+    {
+        'expression': "isodatetime.iso_now(microseconds=True)"
+    },
+    {
+        'expression': "(isodatetime.iso_date_to_datetime('2022-03-04'))"
+    },
 ]
 #fmt:on
 
 def main():
-    # print("isodatetime.iso_now()")
-    # print(f'   {isodatetime.iso_now()}')
-    # print(eval("isodatetime.iso_now()"))
-
     for case in TEST_CASES:
         expression = case['expression']
         print(expression)
-        print(f'   {eval(expression)}')
+        result = eval(expression)
+        if not isinstance(result, str):
+            result = result.__repr__()
+        print(f'   {result}')
 
 
 if __name__ == "__main__":
